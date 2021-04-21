ae3bffac75c634b78c47cec6c0ce4bc094afd7e3
KSHITIJ PAWAR <31236146+khp3040@users.noreply.github.com>
2021-04-21 11:30:41 +0530

    package.json

--- src/server.js
+++ src/server.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const port = process.env.port || 3001;
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname + '/index.html'))
 })
-app.listen(3001, () => console.log('server started'));
+app.listen(port, () => console.log('server started'));
