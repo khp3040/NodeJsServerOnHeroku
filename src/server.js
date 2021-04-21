c4d28aa6f9fbc57c3b2bd1bfd73a8a832b4ad899
KSHITIJ PAWAR <31236146+khp3040@users.noreply.github.com>
2021-04-21 11:45:09 +0530

    nodejs project

--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-const port = process.env.port || 3001;
+const port = process.env.PORT || 3001;
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname + '/index.html'))
 })
